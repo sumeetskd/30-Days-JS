1254d07bbf782e0a58b7050fa402ff3aac514417
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-06-04 03:59:36 +0530

    commit

---
+++ Day26.js
@@ -0,0 +1,19 @@
+//2693. Call Function with Custom Context
+
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+    Object.defineProperty(context, 'fn',{
+        value: this,
+        enumerable: false,
+    });
+    return context.fn(...args);
+}
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
